598625bdb39ca3388470482a73f5e813362b8e30
Hoàng Văn Khoa <khoahv@users.noreply.github.com>
2014-01-10 11:02:06 +0700

    Hoàn thành phương thức ListFile.listFile(File entry) liệt kê đệ quy các tệp và thư mục bắt đầu từ thư mục entry. Phương thức vừa liệt kê ra console vừa liệt kê vào danh sách liên kết đơn KNode

--- ListFile/src/bkav/ListFile.java
+++ ListFile/src/bkav/ListFile.java
@@ -6,21 +6,36 @@ import adt.*;
 
 public class ListFile {
 
-	public static long listFile(File entry) {
-		long numberOfFiles = 0;
+	public static KNode<String> listFile(File entry) {
+		KNode<String> list = new KNode<String>();
 		try {
 			if (!entry.exists()) {
 				System.out.println(entry.getName() + " not found!");
+				return null;
 			}
-			if (entry.isFile()) {
-				System.out.println(entry.getCanonicalPath());
-			} else if (entry.isDirectory()) {
-
+			KStack<File> s = new KStack<File>();
+			File f = new File(entry.getCanonicalPath());
+			s.push(f);
+			while (!s.isEmpty()) {
+				f = s.pop();
+				list.add(f.getAbsolutePath());
+				System.out.println(f.getAbsolutePath());
+				if (f.isDirectory()) {
+					String sarr[] = f.list();
+					for (int i = 0; i < sarr.length; i++) {
+						s.push(new File(f.getAbsolutePath(), sarr[i]));
+						//System.out.println(s.top());
+					}
+				}
 			}
 		} catch (IOException e) {
 			System.err.println("Error: " + e);
+		} catch (FullStackException e) {
+			System.err.println("Error: " + e);
+		} catch (EmptyStackException e) {
+			System.err.println("Error: " + e);
 		}
-		return numberOfFiles;
+		return list;
 	}
 
 	/**
@@ -28,7 +43,11 @@ public class ListFile {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		File entry = new File(".");
+		KNode<?> list = listFile(entry);
+		System.out.println("Number of file(s) and folder(s): " + list.size());
+		for (KNode<?> it = list; !it.isLast(); it = it.getNext()) {
+			System.err.println("[" + it.getNext().getIndex() + ":" + it.getNext().getData() + "]");
+		}
 	}
 }
-
